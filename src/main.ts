1c5b6a3b2e080230972f19b3fc23daf47f90ee4e
Jonas Facon <jonas.facon.etu@univ-lille.fr>
2026-03-07 21:34:08 +0100

    Ajout de la page d'accueil avec styles et fonctionnalités interactives

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,38 @@
-import Router from "./Router.ts";
-import View from "./View.ts";
-import type {Route} from "./types.ts";
-import HomeView from "./HomeView.ts";
+import Router from './Router.ts';
+import View from './View.ts';
+import type { Route } from './types.ts';
+import HomeView from './HomeView.ts';
 
 // Auto-scroll vers la fenêtre de la borne au chargement
 window.addEventListener('load', () => {
-    const fenetre = document.querySelector<HTMLElement>('main.fenetre');
-    if (fenetre) {
-        fenetre.scrollIntoView({ behavior: 'smooth', inline: 'center', block: 'center' });
-    }
+	const fenetre = document.querySelector<HTMLElement>('main.fenetre');
+	if (fenetre) {
+		fenetre.scrollIntoView({
+			behavior: 'smooth',
+			inline: 'center',
+			block: 'center',
+		});
+	}
 });
-
 
 // création des vues de notre application
 const banner = document.querySelector('.fenetre .banner')!;
 const background = document.querySelector('.fenetre .background')!;
 
-const homeView = new HomeView(document.querySelector('.fenetre .home')!, background, banner)
+const homeView = new HomeView(
+	document.querySelector('.fenetre .home')!,
+	background,
+	banner
+);
 const leaderboard = new View(document.querySelector('.fenetre .leaderboard')!);
 const credit = new View(document.querySelector('.fenetre .credit')!);
 const play = new View(document.querySelector('.fenetre .play')!);
 
 const routes: Route[] = [
-    { path: '/', view: homeView, title: 'ShootLévia' },
-    { path: '/leaderboard', view: leaderboard, title: 'LeaderBoar' },
-    { path: '/credit', view: credit, title: 'Crédit' },
-    { path: '/play', view: play, title: '' },
+	{ path: '/', view: homeView, title: 'ShootLévia' },
+	{ path: '/leaderboard', view: leaderboard, title: 'LeaderBoar' },
+	{ path: '/credit', view: credit, title: 'Crédit' },
+	{ path: '/play', view: play, title: '' },
 ];
 
 Router.routes = routes;
@@ -34,9 +41,12 @@ Router.titleElement = document.querySelector('.fenetre .banner .title')!;
 // gestion des liens du menu (détection du clic et activation/désactivation)
 Router.setMenuElement(document.querySelector('.fenetre nav')!);
 
+// enregistrement du bouton "Jouer" comme lien de navigation interne
+Router.registerLinks(
+	document.querySelector<HTMLAnchorElement>('.fenetre .home .playButton')!
+);
+
 // chargement de la vue initiale selon l'URL demandée par l'utilisateur.rice (Deep linking)
 Router.navigate(window.location.pathname, true);
 // gestion des boutons précédent/suivant du navigateur (History API)
 window.onpopstate = () => Router.navigate(document.location.pathname, true);
-
-
