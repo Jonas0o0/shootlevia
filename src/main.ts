9b554ca9328d1d96ef02ee376aedc711bdd6e983
Enzo Dewaele <enzo.dewaele.etu@univ-lille.fr>
2026-03-07 21:55:09 +0100

    prêt pour merge

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import Router from './Router.ts';
 import View from './View.ts';
 import type { Route } from './types.ts';
-import { LeaderboardComponent } from './components/LeaderboardComponent.ts';
-import LeaderboardServiceMemory from './services/LeaderboardServiceMemory.ts';
 
 // Auto-scroll vers la fenêtre de la borne au chargement
 window.addEventListener('load', () => {
@@ -36,14 +34,3 @@ Router.setMenuElement(document.querySelector('.fenetre nav')!);
 Router.navigate(window.location.pathname, true);
 // gestion des boutons précédent/suivant du navigateur (History API)
 window.onpopstate = () => Router.navigate(document.location.pathname, true);
-
-const leaderboardRepo: LeaderboardServiceMemory = new LeaderboardServiceMemory();
-leaderboardRepo.addEntry({ joueur: 'test1', score: 999 });
-leaderboardRepo.addEntry({ joueur: 'test2', score: 100 });
-leaderboardRepo.addEntry({ joueur: 'test3', score: 300 });
-leaderboardRepo.addEntry({ joueur: 'test4', score: 1000 });
-const leaderboardComponent: LeaderboardComponent = new LeaderboardComponent();
-const leaderboardContainer = document.querySelector('.leaderboard-container tbody');
-if (leaderboardContainer) {
-    leaderboardContainer.innerHTML = leaderboardComponent.render(leaderboardRepo.getEntries());
-}
