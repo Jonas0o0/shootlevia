45fd9a427bc6a5c1ca2c431d27d5216497d54658
Enzo Dewaele <enzo.dewaele.etu@univ-lille.fr>
2026-03-08 21:07:53 +0100

    Test : crée une lifebar avec par default 3 vies et le nombre que l'on veut en paramètre

---
+++ src/tests/services/LifebarService.test.ts
@@ -0,0 +1,17 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { LifebarService } from '../../services/LifebarService.ts';
+
+describe('default constructeur', () => {
+	it('il devrait avoir que 3 vie', () => {
+		const lifebar = new LifebarService();
+		assert.strictEqual(lifebar.life, 3);
+	});
+});
+
+describe('constructeur avec un nombre de vie donné', () => {
+	it('il devrait avoir 10 vie', () => {
+		const lifebar = new LifebarService(10);
+		assert.strictEqual(lifebar.life, 10);
+	});
+});
