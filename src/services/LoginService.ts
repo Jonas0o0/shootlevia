f7ee45cef5bf8ca04686bcd6c4d7a91603359474
Jonas Facon <jonas.facon.etu@univ-lille.fr>
2026-03-09 09:26:21 +0100

    Ajout de l'interface LoginService pour la gestion de l'authentification

---
+++ src/services/LoginService.ts
@@ -0,0 +1,15 @@
+import type { Account } from '../types.ts';
+
+export interface LoginService {
+	isLoggedIn(): boolean;
+
+	login(account: Account): void;
+
+	logout(): void;
+
+	getCurrentUser(): Account | null;
+
+	checkCoookie(): Account | null;
+
+	setCookie(account: Account): void;
+}
