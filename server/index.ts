a2655b6c24c8fc2fb1eed01d5deae2507b73b95e
Jonas Facon <jonas.facon.etu@univ-lille.fr>
2026-03-14 11:57:37 +0100

    feat(server): intégration de ServerGame et gestion des événements socket

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import { Server as IOServer } from 'socket.io';
 import { loadEnvFile } from 'node:process';
 import leaderboardRoutes from './routes/leaderboardRoutes';
+import { ServerGame } from './models/Game';
+import { Direction } from '../common/Direction';
 
 // Chargement des variables d'environnement
 loadEnvFile('.env');
@@ -20,9 +22,34 @@ app.use('/leaderboard', leaderboardRoutes);
 const server = http.createServer(app);
 const io = new IOServer(server, { cors: { origin: true } });
 
+const game = new ServerGame();
+
 io.on('connection', socket => {
 	console.log('Client connected:', socket.id);
+
+	// On pourrait recevoir les infos du joueur lors de la connexion
+	socket.on('join', (data: { username: string; avatar: string }) => {
+		game.addPlayer(socket.id, data.username, data.avatar);
+	});
+
+	socket.on('move', (directions: Direction[]) => {
+		game.handlePlayerMove(socket.id, directions);
+	});
+
+	socket.on('jump', () => {
+		game.handlePlayerJump(socket.id);
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Client disconnected:', socket.id);
+		game.removePlayer(socket.id);
+	});
 });
+
+// Broadcast de l'état du jeu toutes les 16ms (~60fps)
+setInterval(() => {
+	io.emit('gameState', game.getState());
+}, 1000 / 60);
 
 // Lancement du serveur
 server.listen(port, () => {
