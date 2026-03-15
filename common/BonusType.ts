ff5d731c25302ae23998d7c7926b8467864e2836
Jonas Facon <jonas.facon.etu@univ-lille.fr>
2026-03-15 17:09:32 +0100

    Création des bonus #35

---
+++ common/BonusType.ts
@@ -0,0 +1,14 @@
+export const BonusType = {
+	Shield: {
+		nom: 'PassPass',
+		bonusType: 'SHIELD',
+		sprite: 'PASS_PASS',
+		rows: {
+			HUD: 0,
+			MAP: 1,
+			PLAYER: 2,
+		}
+	},
+} as const;
+
+export type BonusType = (typeof BonusType)[keyof typeof BonusType];
