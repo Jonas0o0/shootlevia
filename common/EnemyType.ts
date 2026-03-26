29bb636d603185c0529d5eec90fb89c5d4f15093
Jonas Facon <jonas.facon.etu@univ-lille.fr>
2026-03-26 10:19:07 +0100

    changement du type des enemies

--- common/EnemyType.ts
+++ common/EnemyType.ts
@@ -5,7 +5,6 @@ export interface EnemyConfig {
 	baseHealth: number;
 	spawnProbability: number; // Probabilité relative de spawn
 	scoreValue: number;
-	damageOnHit: number; // Dégâts subis par l'ennemi par balle
 }
 
 export const EnemyConfigs: Record<EnemyType, EnemyConfig> = {
@@ -14,13 +13,11 @@ export const EnemyConfigs: Record<EnemyType, EnemyConfig> = {
 		baseHealth: 160,
 		spawnProbability: 0.3,
 		scoreValue: 10,
-		damageOnHit: 11,
 	},
 	DRONE: {
 		type: 'DRONE',
 		baseHealth: 30,
 		spawnProbability: 0.7,
 		scoreValue: 20,
-		damageOnHit: 16,
 	},
 };
